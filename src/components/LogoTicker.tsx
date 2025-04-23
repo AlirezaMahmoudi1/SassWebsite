c8183ea74b87905b73020ef23053143c0c0ff158
alireza <mlyrda933@gmail.com>
2025-04-23 14:49:07 +0330

    update

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -22,7 +22,7 @@ export const LogoTicker = () => {
     <div className="bg-black text-white py-[72px] sm:py-24 ">
       <div className="container">
         <h2 className="text-xl text-center text-white/70  ">
-          Trusted by the world's most innovative teams
+          Trusted by the world&apos;s most innovative teams
         </h2>
         <div className=" flex overflow-hidden mt-9 before:z-10 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
           <motion.div
@@ -36,16 +36,16 @@ export const LogoTicker = () => {
             className="flex flex-none gap-16 pr-16"
           >
             {images.map(({ src, alt }) => (
-              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+              <Image src={src} alt={alt} key={alt} className="flex-none h-8 w-auto" />
             ))}
             {images.map(({ src, alt }) => (
-              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+              <Image src={src} alt={alt} key={alt} className="flex-none h-8 w-auto" />
             ))}
             {images.map(({ src, alt }) => (
-              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+              <Image src={src} alt={alt} key={alt} className="flex-none h-8 w-auto" />
             ))}
             {images.map(({ src, alt }) => (
-              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+              <Image src={src} alt={alt} key={alt} className="flex-none h-8 w-auto" />
             ))}
           </motion.div>
         </div>
